67dcf64d8e1f6e4357f4d14d28ba2e4307f93a10
Manlikili <165214712+Manlikili@users.noreply.github.com>
2025-02-17 08:49:33 +1100

    Update 安装.py

--- 安装.py
+++ 安装.py
@@ -1,9 +1,12 @@
 #选择conda安装环境
-conda env remove d2l-zh
- conda create -n -y d2l-zh python=3.8 pip conda activate d2l-zh
+conda env remove --name d2l-zh
+conda create -n -y d2l-zh python=3.8 pip -y
+conda activate d2l-zh
 #安装需要的包
- pip install -y jupyter d2l torch torchvision
+ pip install jupyter d2l torch torchvision 
 #下载代码并执行
+brew --version
+brew install wget
  wget https://zh-v2.d2l.ai/d2l-zh.zip 
  unzip d2l-zh.zip
  jupyter notebook
