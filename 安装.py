c962c98aed72fdb34056cec161a02129fd2e015a
Manlikili <165214712+Manlikili@users.noreply.github.com>
2025-02-17 18:38:55 +1100

    Update 安装.py

--- 安装.py
+++ 安装.py
@@ -10,4 +10,6 @@ brew install wget
  wget https://zh-v2.d2l.ai/d2l-zh.zip 
  unzip d2l-zh.zip
  jupyter notebook
-
+## 请始终执行conda activate d2l以激活运行时环境。 要退出环境，请运行conda deactivate。
+conda activate d2l
+conda deactivate
